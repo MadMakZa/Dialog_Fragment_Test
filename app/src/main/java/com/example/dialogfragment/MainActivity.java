8b9655d3ff440f13ef56d77ded4ab7d0be9e32ea
MadMakZa <immakza@gmail.com>
2021-01-03 19:54:15 +0300

    init block

--- app/src/main/java/com/example/dialogfragment/MainActivity.java
+++ app/src/main/java/com/example/dialogfragment/MainActivity.java
@@ -2,21 +2,28 @@ package com.example.dialogfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button openDialog;
+    public static final String TAG = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         init();
     }
     void init(){
         openDialog = findViewById(R.id.btn_opendialog);
+        MainFragment fragment = new MainFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment, "MainFragment");
+
     }
     public void onClickOpenDialog(View view){
         //метод нажатия на кнопку
---
+++ app/src/main/java/com/example/dialogfragment/MainFragment.java
@@ -0,0 +1,24 @@
+package com.example.dialogfragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+public class MainFragment extends Fragment {
+
+    public static final String TAG = "Main Fragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+    }
+}
